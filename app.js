578b5dbd381ae4b583dc9a033808c8ee08976a92
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-16 12:56:00 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let name = "Wiktor";
+let lastName = "Sobczak";
+console.log(name + " " + lastName);
+
+name = "Jan";
+lastName = "Kowalski";
+console.log(name + " " + lastName);
+
+let person1 = {
+    name: "Wiktor",
+    lastName: "Sobczak"
+};
+console.log(person1.name + " " + person1.lastName);
+
+ let person2 = {
+     name: "Jan",
+     lastName: "Kowalski"
+ };
+ console.log = (person2.name + " " + person2.lastName)
