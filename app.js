8aaae3e3f3168d9590f027307735b5c391c4dabd
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-18 14:18:12 +0200

    added addEvenListener click

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 let arrowLeft = document.querySelector(".arrow-left");
-console.log(arrowLeft);
 let arrowRight = document.querySelector(".arrow-right");
-console.log(arrowRight);
+
 
 let dot1 = document.querySelector("#dot1");
-console.log(dot1);
 let dot2 = document.querySelector("#dot2");
-console.log(dot2);
 let dot3 = document.querySelector("#dot3");
-console.log(dot3);
+
 
 let slide1 = document.querySelector("#slide1");
-console.log(slide1);
 let slide2 = document.querySelector("#slide2");
-console.log(slide2);
 let slide3 = document.querySelector("#slide3");
-console.log(slide3);
+
+dot2.addEventListener("click");
