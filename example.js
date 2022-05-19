1a9a9cc7ae54879e792c2cbdedf07c5f18d8b83b
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-19 15:26:21 +0200

    added funcion in to dot chenged in app.js

---
+++ example.js
@@ -0,0 +1,7 @@
+let myFirstFunction = (name, greeting) => {
+  console.log(greeting + "! mam na imię " + name + "!")  
+};
+
+myFirstFunction("wiktor", "Hej");
+myFirstFunction("Jan", "Dzień dobry");
+
