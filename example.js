cefc4f8aecb15d98b9dae096161d325dc90f0101
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-20 13:05:23 +0200

    train boolean added if and else in to example.js

--- example.js
+++ example.js
@@ -1,15 +1,25 @@
 let bool1 = 5 > 2;
-console.log(bool1)
+console.log(bool1);
 
 let bool = 5 > 10;
-console.log(bool)
+console.log(bool);
 
 let bool3 = 1 === 1;
-console.log(bool3)
+console.log(bool3);
 
 let bool4 = 1 !== 2;
-console.log(bool4)
+console.log(bool4);
 
 let bool5 = "Wiktor" === "Wiktor";
-console.log(bool5)
+console.log(bool5);
 
+let isBiggerThan10 = (number) => {
+    if(number > 10) {
+        console.log("Liczba jest wieksza od 10");
+    }
+    else {
+        console.log("Liczba nie jest wieksza od 10");
+    }
+};
+
+isBiggerThan10(11); 
